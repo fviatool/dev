0c76a8aff5201b9bbb13b6b2a582278445c7a0ed
FVIA <130783224+fviatool@users.noreply.github.com>
2023-12-20 13:39:18 +0700

    Update xa.sh

--- xa.sh
+++ xa.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
 
-# Function to generate a new IPv4 address
-get_new_ipv4() {
-    random_ipv4=$(echo "$((RANDOM%256))"."$((RANDOM%256))"."$((RANDOM%256))"."$((RANDOM%256))")
-    echo "$random_ipv4"
-}
-
 # Function to generate a new IPv6 address
 get_new_ipv6() {
     random_ipv6=$(openssl rand -hex 8 | sed 's/\(..\)/:\1/g; s/://1')
     echo "$random_ipv6"
 }
 
-# Function to update 3proxy configuration with a new IPv4 address
-update_3proxy_ipv4_config() {
-    new_ipv4=$1
-    sed -i "s/old_ipv4_address/$new_ipv4/" /usr/local/etc/3proxy/3proxy.cfg
-}
 
 # Function to update 3proxy configuration with a new IPv6 address
 update_3proxy_ipv6_config() {
@@ -27,17 +16,8 @@ update_3proxy_ipv6_config() {
 # Function to restart 3proxy
 restart_3proxy() {
     /usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
-}
-
-echo "Rotating IPv4 addresses for 3proxy every 20 minutes..."
-
-# Get a new IPv4 address and update 3proxy configuration
-new_ipv4=$(get_new_ipv4)
-update_3proxy_ipv4_config "$new_ipv4"
-
+}l
 restart_3proxy
-
-echo "IPv4 rotation for 3proxy completed."
 
 echo "Rotating IPv6 addresses for 3proxy every 10 minutes..."
 
